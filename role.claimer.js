7bbd564bc6e2a9ed207105859abc159c25c86bae
Hamza Akiour <contacto@hamza.es>
2018-07-11 01:18:02 +0200

    Updated role.claimer.js optimizing the whole code, now the creep find the room whit their memory (we set the memory target on the spawning function inside masterSpawner.js), without any flag.

--- role.claimer.js
+++ role.claimer.js
@@ -3,55 +3,16 @@ var roleClaimer = {
    
     /** @param {Creep} creep **/
     run: function(creep) {
-
-		function moveToObjective(){
-			if(!creep.memory.onTheObjective){
-				if(!creep.memory.onFlag || creep.memory.onFlag == undefined && creep.ticksToLive > 10){
-				 	for(var thisFlag in Game.flags) {       
-				        var name = Game.flags[thisFlag].name;
-				        if (name.startsWith("ClaimFlag") && (Game.flags[thisFlag].memory.bussy == false || Game.flags[thisFlag].memory.bussy == undefined)){
-				            Game.flags[thisFlag].memory.bussy = true;
-				            creep.memory.onFlag = name;
-				            creep.moveTo(Game.flags[thisFlag], {visualizePathStyle: {stroke: '#ffaa00'}});
-				            break;
-				        }
-					}
-				}else if(creep.pos.getRangeTo(Game.flags[creep.memory.onFlag]) <= 1){
-	            	creep.memory.onTheObjective = true;
-	            }else{
-	            	creep.moveTo(Game.flags[creep.memory.onFlag]);
-	            }
+    	if (creep.memory.target == creep.room.name){
+    		// Try to claim the controller
+            if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
 			}
-		}
-
-        function findNearClaimFlag(){
-
-        	if(!creep.memory.onTheObjective){
-        		moveToObjective();
-        	}else{
-        		claimSpot();
-        	}	
-        }
-
-        function claimSpot(){
-			if(creep.room.controller) {
-				var claimControllerResult = creep.claimController(creep.room.controller);	
-				if(claimControllerResult == ERR_NOT_IN_RANGE) {
-			        creep.moveTo(creep.room.controller);
-			    }else if(claimControllerResult == ERR_GCL_NOT_ENOUGH){
-			    	creep.reserveController(creep.room.controller);
-			    }
-			}else{
-				creep.memory.onTheObjective = false;
-				creep.memory.onFlag = false;
-				findNearClaimFlag();
-			}
-        }
-		if(creep.ticksToLive < 10 && creep.memory.onFlag){
-            Game.flags[creep.memory.onFlag].memory.bussy = false;
-            creep.suicide();
-		}
-        findNearClaimFlag();
+    	}else{
+    		//Move to the target room
+    		var exitToTarget = creep.room.findExitTo(creep.memory.target);
+			creep.moveTo(creep.pos.findClosestByRange(exitToTarget));
+    	}
 	}
    
 };
