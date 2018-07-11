01b31db6672017feeddd987efaf3c3b4ff4eacce
Hamza Akiour <contacto@hamza.es>
2018-07-11 21:01:29 +0200

    Optimiced role.minerjs , now miners work with memory, not flags.

--- role.miner.js
+++ role.miner.js
@@ -3,45 +3,17 @@ var roleMiner = {
    
     /** @param {Creep} creep **/
     run: function(creep) {
-        var maxTicksToLive = 5;
-			if(creep.ticksToLive > maxTicksToLive && (!creep.memory.onFlag || creep.memory.onFlag == undefined)){
-                //Find some flag that is not bussy and assing it to the miner (ordered by distance). Once is assigned we mark it as bussy
-                //I don't use this method because is so expensive for the CPU, once they are +7 miners, CPU got crazy
-                /*for(var thisFlag of _.sortBy(Game.flags, flag => creep.pos.findPathTo(flag.pos))) {  
-                    var name = thisFlag.name;
-                    if (name.startsWith("EnergyFlag") && (thisFlag.memory.bussy == false || thisFlag.memory.bussy == undefined)){
-                        thisFlag.memory.bussy = true;
-                        creep.memory.onFlag = name;
-                        creep.moveTo(Game.flags[thisFlag]);
-                        break;
-                    }
-                }*/
-                //Find some flag that is not bussy and assing it to the miner. Once is assigned we mark it as bussy
-                    for(var thisFlag in Game.flags) {       
-                    var name = Game.flags[thisFlag].name;      
-                    if (name.startsWith("EnergyFlag") && (Game.flags[thisFlag].memory.bussy == false || Game.flags[thisFlag].memory.bussy == undefined)){
-                        Game.flags[thisFlag].memory.bussy = true;
-                        creep.memory.onFlag = name;
-                        creep.moveTo(Game.flags[thisFlag]);
-                        break;
-                    }
-                }
-                
-            }else{
-                if(creep.ticksToLive <= maxTicksToLive){ //If we gonna die, assing the flag as no bussy and die peaceful
-                    console.log("Is this running? I think it's running!" + creep.memory.onFlag);
-                    Game.flags[creep.memory.onFlag].memory.bussy = false;
-                    creep.memory.onFlag = true;
-                    creep.suicide();
-
-                } else if(creep.pos.getRangeTo(Game.flags[creep.memory.onFlag]) <= 1){ //if we are near to our assigned flag...
-						genericFunctions.harvestNearSource(creep);
-            	}
-            	else{
-            		 creep.moveTo(Game.flags[creep.memory.onFlag]);
-            	}
+        if (creep.memory.target == creep.room.name){
+            //Harvest the source associated to this miner
+            let sources = Game.getObjectById(creep.memory.energySource);
+            if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-	    
+        }else{
+            //Move to the target room
+            let exitToTarget = creep.room.findExitTo(creep.pos.findClosestByRange(creep.memory.target));
+            creep.moveTo(exitToTarget);
+        }
 	}
    
 };
