3d7be5ba34f9535727ec113be8ad755b06f7e672
Hamza Akiour <contacto@hamza.es>
2018-07-11 23:14:57 +0200

    Revert "Fixed minor error on miners that block they self in the roomRoot"
    This reverts commit 215078f11ec99f009d36747768fdeff7deb02c1f.

--- role.miner.js
+++ role.miner.js
@@ -3,7 +3,7 @@ var roleMiner = {
    
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.memory.target != creep.room.name){
+        if (creep.memory.target == creep.room.name){
             //Harvest the source associated to this miner
             let sources = Game.getObjectById(creep.memory.energySource);
             if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
